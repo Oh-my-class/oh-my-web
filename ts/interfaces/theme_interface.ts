adec0f150d7d2e51aafbb0b224c2353967dce955
Thierry Pfister <tpfister@kpmg.com>
2022-09-06 14:50:42 +0200

    user interface has username, mail and theme Theme interface

---
+++ ts/interfaces/theme_interface.ts
@@ -0,0 +1,4 @@
+export interface theme {
+    darkmode: true,
+    lightmode: false
+}
---
+++ ts/interfaces/user_interface.ts
@@ -0,0 +1,7 @@
+import { theme } from "./theme_interface";
+
+export interface user {
+    username: string,
+    mail: string,
+    theme: theme,
+}
