89b16c0277d41c7d61280fee3d0f5030acf8e60f
Thierry Pfister <tpfister@kpmg.com>
2022-09-13 14:39:38 +0200

    Handy view is designed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from "next";
 import React from "react";
 
 const Home: NextPage = () => {
-  return <div data-theme="cupcake" className=" w-screen h-screen absolute flex flex-row justify-center bg-base-100 font-sans"> 
+  return <div data-theme="winter" className=" w-screen h-screen absolute flex flex-row justify-center bg-base-100 font-sans bg-gradient-to-t from-white via-white to-slate-600"> 
     <div className=" w-9/12 text-center mt-14">
-      <h1 className=" text-slate-800 font-bold text-4xl">Oh my Class</h1>
+      <img src="https://www.theanimator.co.in/icon/1998840123schoolmanagement-icon.png" alt="" />
+      <h1 className=" text-slate-800 font-bold text-5xl">Oh my Class</h1>
       <p className=" text-slate-600 text-xl font-bold mt-7">Make your school life at least better than suicidal thoughts</p>
-      <button className="btn-primary btn-wide p-4 mt-7 rounded-2xl">Go to page</button>
+      <button className="btn-primary btn-wide p-4 mt-7 rounded-2xl"><span className="text-lg font-bold">Go to page</span></button>
     </div>
   </div>;
 };
