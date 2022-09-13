a1f2bd79bec22330a6f50d35daa5e3137b7deb87
Thierry Pfister <tpfister@kpmg.com>
2022-09-13 15:02:58 +0200

    Laptop view is designed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Home: NextPage = () => {
   return <div data-theme="winter" className=" w-screen h-screen absolute flex flex-row justify-center bg-base-100 font-sans bg-gradient-to-t from-white via-white to-slate-600"> 
-    <div className=" w-9/12 text-center mt-14">
-      <img src="https://www.theanimator.co.in/icon/1998840123schoolmanagement-icon.png" alt="" />
+    <div className=" w-9/12 text-center mt-14 md:mt-8">
+      <img src="https://www.theanimator.co.in/icon/1998840123schoolmanagement-icon.png" alt="School Icon" className="md:w-1/4 md:h-1/8 mx-auto"/>
       <h1 className=" text-slate-800 font-bold text-5xl">Oh my Class</h1>
-      <p className=" text-slate-600 text-xl font-bold mt-7">Make your school life at least better than suicidal thoughts</p>
+      <p className=" text-slate-600 text-xl font-bold mt-7">Make your school life at least better than suicidal thoughts!</p>
       <button className="btn-primary btn-wide p-4 mt-7 rounded-2xl"><span className="text-lg font-bold">Go to page</span></button>
     </div>
   </div>;
