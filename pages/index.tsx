dd964bd8437ea69892d3ae7bf1e1f0186d6cf61c
Thierry Pfister <tpfister@kpmg.com>
2022-09-13 10:36:28 +0200

    Removed sus button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import React from "react";
 
 const Home: NextPage = () => {
-  return <button className="btn">Kinda sus</button>;
+  return <div></div>;
 };
 
 export default Home;
