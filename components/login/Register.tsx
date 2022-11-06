f8bce9edef25cc3cfea88e84f89ae0ca295b19aa
Lenny Angst <lenny@familie-angst.ch>
2022-11-06 21:03:21 +0100

    🛂 Add registration api call

--- components/login/Register.tsx
+++ components/login/Register.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
-import React, { FormEventHandler, useRef } from "react";
+import React, { FormEventHandler, useRef, useState } from "react";
 import { Alert, AlertType } from "../../ts/interfaces/Alert";
 import { useAlertStore } from "../../ts/store/AlertStore";
 
 export const Register: React.FC = () => {
   const form = useRef<HTMLFormElement>(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const alertStore = useAlertStore();
 
   function validate() {
     return form.current?.reportValidity();
   }
 
-  const submit: FormEventHandler<HTMLFormElement> = (e) => {
+  const submit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     if (!validate()) {
       alertStore.addAlert(
@@ -23,10 +27,32 @@ export const Register: React.FC = () => {
       );
       return;
     }
+
+    // TODO centralize API somewhere
+    const response = await fetch("http://localhost:8080/api/v1/auth/register", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ username, email, password }),
+    });
+
+    if (response.status !== 200) {
+      alertStore.addAlert(
+        new Alert(
+          "Registration failed",
+          "Your registration could not be finished. Are you already registered or is your username already taken?",
+          AlertType.error
+        )
+      );
+      return;
+    }
+
     alertStore.addAlert(
-      new Alert("Amogus", "Schono sus was du da machsch", AlertType.error)
+      new Alert(
+        "Registration successful",
+        "Thanks for choosing Oh my Class!",
+        AlertType.success
+      )
     );
-    // TODO send register request
   };
 
   return (
@@ -48,6 +74,7 @@ export const Register: React.FC = () => {
               className="input input-primary w-full max-w-xs"
               minLength={6}
               maxLength={32}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="form-control max-w-xs">
@@ -59,6 +86,7 @@ export const Register: React.FC = () => {
               type="email"
               placeholder="john.doe@ohmyclass.ch"
               className="input input-primary w-full max-w-xs"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-control w-full max-w-xs">
@@ -69,6 +97,7 @@ export const Register: React.FC = () => {
               required
               type="password"
               className="input input-primary w-full max-w-xs"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
